0dba840fa540eeb71f67712e15db0131c612caa4
Ratna selvan <ratnaselvan.iqtechmax@gmail.com>
2025-05-10 19:37:32 +0530

    episode three completed on 10-05-2025 7:40

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child", key: "1" },
-    React.createElement("h1", {}, "this is heading")
-  ),
-  React.createElement(
-    "div",
-    { id: "child 2", key: "2" },
-    React.createElement("h1", {}, "this is another heading")
-  ),
-]);
+
+const varia = 100;
+
+const Heading = () => {
+  return <h1 className="heading">This is the heading</h1>;
+};
+
+const HadingComponent = () => {
+  return (
+    <div>
+      <Heading />
+      {varia}
+      <h3 className="not_a_heading">
+        This is just another heading ❤️❤️❤️❤️❤️🚀🚀🚀
+      </h3>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HadingComponent />);
