3c6b9bccf513d5336b72a79d3fcb4a6dfb9690ef
Ratna selvan <ratnaselvan.iqtechmax@gmail.com>
2025-05-10 15:06:51 +0530

    Episode 2 parcel and npm react

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
-    { id: "child" },
+    { id: "child", key: "1" },
     React.createElement("h1", {}, "this is heading")
   ),
   React.createElement(
     "div",
-    { id: "child 2" },
+    { id: "child 2", key: "2" },
     React.createElement("h1", {}, "this is another heading")
   ),
 ]);
