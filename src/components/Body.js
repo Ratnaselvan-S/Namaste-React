3e5238a3f8d48c1418c41c03f7c7dfe0b54d278f
Ratna selvan <ratnaselvan.iqtechmax@gmail.com>
2025-05-14 19:56:09 +0530

    completed on 14-05-2025

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,24 @@
 import { resList } from "./utils/constData";
 import RestaurantCard from "./RestaurantCard";
+import React, { useState } from "react";
 
 const Body = () => {
+  const [arrayData, setarrayData] = useState(resList);
   return (
     <div className="body">
       <div className="search-container">
         <input type="text" placeholder="Search Food or Restaurant" />
-        <button>Search</button>
+        <button
+          onClick={() => {
+            const filter = arrayData.filter((data) => data.data.avgRating > 4);
+            setarrayData(filter);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {arrayData.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
