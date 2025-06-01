0ea9a731673ddb598be3145c6991150fe42b07e5
Ratna selvan <ratnaselvan.iqtechmax@gmail.com>
2025-06-01 11:03:26 +0530

    just starting from new

--- src/components/__test__/sum.test.js
+++
@@ -1,6 +0,0 @@
-import { sum } from "../sum";
-
-test("sum funtion is tested", () => {
-  const result = sum(3, 4);
-  expect(result).toBe(7);
-});
--- src/utils/UserContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const UserContest = createContext({
-  loggedinuser: "default user",
-});
-
-export default UserContest;
