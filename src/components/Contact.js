31bb0a3d5ceddb530ecdea5923a3bcb45c96bf98
Ratna selvan <ratna@iqtechmax.com>
2025-07-19 19:34:23 +0530

    youtube search and comments also live comments

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,117 @@
+import { useState } from "react";
+
+const COMMENTS = [
+  {
+    name: "Ratna",
+    message: "I am a super start",
+    replies: [
+      {
+        name: "Ratna",
+        message: "I am a super start",
+        replies: [
+          { name: "Ratna", message: "I am a super start" },
+          { name: "Ratna", message: "I am a super start" },
+          { name: "Ratna", message: "I am a super start" },
+          { name: "Ratna", message: "I am a super start" },
+        ],
+      },
+      { name: "Ratna", message: "I am a super start" },
+      { name: "Ratna", message: "I am a super start" },
+      {
+        name: "Ratna",
+        message: "I am a super start",
+        replies: [
+          {
+            name: "Ratna",
+            message: "I am a super start",
+            replies: [
+              {
+                name: "Ratna",
+                message: "I am a super start",
+                replies: [
+                  {
+                    name: "Ratna",
+                    message: "I am a super start",
+                    replies: [
+                      {
+                        name: "Ratna",
+                        message: "I am a super start",
+                        replies: [
+                          {
+                            name: "Ratna",
+                            message: "I am a super start",
+                            replies: [
+                              { name: "Ratna", message: "I am a super start" },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+              { name: "Ratna", message: "I am a super start" },
+            ],
+          },
+        ],
+      },
+      { name: "Ratna", message: "I am a super start" },
+    ],
+  },
+  { name: "Ratna", message: "I am a super start" },
+  { name: "Ratna", message: "I am a super start" },
+];
+
+const Comments = ({ data }) => {
+  const { name, message } = data;
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: " rgb(201, 200, 200)",
+        margin: "3px",
+        width: "50%",
+      }}
+    >
+      <img
+        src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+        height={"50px"}
+        width={"50px"}
+      />
+      <div>
+        <h3>{name}</h3>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ comments }) => {
+  const [showreplies, setShowReplies] = useState(false);
+  return comments.map((data, index) => {
+    return (
+      <div>
+        <Comments data={data} />
+        <div style={{ borderLeft: "1px solid black", paddingLeft: "20px" }}>
+          {data.replies && (
+            <div onClick={() => setShowReplies(true)}>
+              <p style={{ cursor: "pointer" }}>
+                {!showreplies && "Show replies"}
+              </p>
+
+              {showreplies && <CommentList comments={data.replies} />}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  });
+};
+
 const Contact = () => {
   return (
     <div>
-      <h2>Contact us page</h2>
-      <h5>Namaste react </h5>
+      <CommentList comments={COMMENTS} />
     </div>
   );
 };
